7bd9cffdf76512700926bcef6abf1f0ff2ddad36
napatoxd <164836327+napatoxd@users.noreply.github.com>
2024-10-08 14:04:22 +0700

    Delete cell04 directory

--- cell04/ex00/upcase.py
+++
@@ -1,5 +0,0 @@
-def main():
-    user_input = input("Give me a word: ")
-    print(user_input.upper())
-
-main()
--- cell04/ex05/up_low.py
+++
@@ -1,8 +0,0 @@
-def swap_case(s):
-    return s.swapcase()
-
-user_input = input("Please enter a string: ")
-    
-swapped_string = swap_case(user_input)
-    
-print("The modified string is:", swapped_string)
