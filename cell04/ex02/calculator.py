c94de1f03916acc847759686a429a41cecee8cd3
napatoxd <164836327+napatoxd@users.noreply.github.com>
2024-10-08 12:20:24 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+FN = int(input("Give me first number :"))
+SN = int(input("Give me secound number :"))
+def cal():
+    print("Thank you ")
+    print(f"{FN} + {SN} = ",FN+SN)
+    print(f"{FN} - {SN} = ",FN-SN)
+    print(f"{FN} / {SN} = ",FN/SN)
+    print(f"{FN} * {SN} = ",FN*SN)
+
+cal()
