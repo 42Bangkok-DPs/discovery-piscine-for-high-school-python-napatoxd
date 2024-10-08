2227bf8c6124bbb10fc569907a5dff298bbd91c5
napatoxd <164836327+napatoxd@users.noreply.github.com>
2024-10-08 14:04:15 +0700

    Delete cell03 directory

--- cell03/ex00/to25.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter a number less than 25  "))
-while True :
-    if number >= 25 :
-        print("Error")
-        break
-    else :
-        number = number +1
-        print("Inside the loop, my variable is" , number)
-
--- cell03/ex02/i_got_that.py
+++
@@ -1,6 +0,0 @@
-WORD = input("What you gotta say? :")
-while True :
-    if WORD == "STOP" :
-        break
-    else :
-        WORD = input("I got that! Anything else? : ")
